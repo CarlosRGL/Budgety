84f543172d1c321fb2159e9841c4c208dbe2f303
CarlosRGL <carlosruizgl@gmail.com>
2018-04-12 15:25:51 +0200

    Added modules controllers

---
+++ app.js
@@ -0,0 +1,14 @@
+// Budget Controller
+
+var budgetController = (function() {})();
+
+// UI Controller
+
+var UIController = (function() {})();
+
+// Controller
+
+var controller = (function(budgetCtrl, UIctrl) {})(
+  budgetController,
+  UIController
+);
