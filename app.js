569fdc02fe6e896218b7c7f918e221a5160df491
CarlosRGL <carlosruizgl@gmail.com>
2018-04-12 15:38:50 +0200

    setup eventListener to add input button and ENTER key

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // Budget Controller
-
 var budgetController = (function() {})();
 
 // UI Controller
-
 var UIController = (function() {})();
 
 // Controller
+var controller = (function(budgetCtrl, UIctrl) {
+  var ctrlAddItem = function() {
+    // 1. Get the filed input data
+    // 2. Add item to budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget
+  };
 
-var controller = (function(budgetCtrl, UIctrl) {})(
-  budgetController,
-  UIController
-);
+  //click event
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  //key press event
+  document.addEventListener('keypress', function(event) {
+    // check if ENTER key is pressed
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
