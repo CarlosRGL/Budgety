44fa2fc6b6f4df95ebb37e7dcc858b878d3f12e5
CarlosRGL <carlosruizgl@gmail.com>
2018-04-16 14:37:19 +0200

    Setup Modules controllers

---
+++ app.js
@@ -0,0 +1,8 @@
+// Budget Controller
+const budgetController = (() => {})();
+
+//UI Controller
+const UIController = (() => {})();
+
+// App Controller
+const controller = (() => {})(budgetController, UIController);
