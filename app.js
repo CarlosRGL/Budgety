0afe1e759de07689ac0ff9f1af6c7ed0f412ae79
CarlosRGL <carlosruizgl@gmail.com>
2018-04-16 14:48:30 +0200

    added query selectors and ctrlAddItem function

--- app.js
+++ app.js
@@ -5,4 +5,24 @@ const budgetController = (() => {})();
 const UIController = (() => {})();
 
 // App Controller
-const controller = (() => {})(budgetController, UIController);
+const controller = ((budgetCtrl, UICtrl) => {
+  const ctrlAddItem = () => {
+    // 1. Get the filed input data
+    // 2. Add item to budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget
+  };
+
+  document
+    .querySelector('.add__btn')
+    .addEventListener('click', function(event) {
+      ctrlAddItem();
+    });
+
+  document.addEventListener('keypress', event => {
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
