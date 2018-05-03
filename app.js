600b706f5d950f2863bf6915a7b1ca799fb5c17a
CarlosRGL <carlosruizgl@gmail.com>
2018-05-03 16:00:45 +0200

    remove modules

--- app.js
+++ app.js
@@ -1,5 +1,82 @@
-import { budgetController } from './modules/budgetController.js';
-import { UIController } from './modules/UIController.js';
+// Budget Controller
+const budgetController = (() => {
+  // Create Expenses Class
+  class Expenses {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  // Create Income Class
+  class Incomes {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  // Array to store Data
+  const data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: (type, des, val) => {
+      let ID, newItem;
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id - 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === 'exp') {
+        newItem = new Expenses(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Incomes(ID, des, val);
+      }
+
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
+})();
+
+//UI Controller
+const UIController = (() => {
+  const DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: () => {
+      return DOMstrings;
+    }
+  };
+})();
 
 const controller = ((budgetCtrl, UICtrl) => {
   // Setup events listenners for init functions
