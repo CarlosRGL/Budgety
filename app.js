c84eb0823d1347c4dd4605bdf6c1baa6e08e4c68
CarlosRGL <carlosruizgl@gmail.com>
2019-10-09 11:21:38 +0200

    📦 NEW: added event listener

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {})();
 
+// UI CONTROLLER
 var UIController = (function() {})();
 
-var controller = (function(budgetCtrl, UICtrl) {})(
-  budgetController,
-  UIController
-);
+// CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  };
+  document.querySelector('.add__btn ').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
