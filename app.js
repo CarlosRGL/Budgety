e893c726ceedcc044f511b0dfe114ff4ddc9a25c
CarlosRGL <carlosruizgl@gmail.com>
2019-10-09 14:17:05 +0200

    👌 IMPROVE: create data logic

--- app.js
+++ app.js
@@ -21,6 +21,35 @@ var budgetController = (function() {
       inc: 0
     }
   };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp'
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push into or data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 // UI CONTROLLER
@@ -59,9 +88,15 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
     // 1. Get the field input data
-    var input = UICtrl.getinput();
+    input = UICtrl.getinput();
     // 2. Add the item to the budget controller
+    newItem = budgetController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
