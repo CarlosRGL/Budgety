d5cb426fbd2fd59e75d83e08453196387fbe1736
carlosrgl <carlos@vernalis.fr>
2019-10-23 14:42:17 +0200

    📦 NEW: delete item from data model

--- app.js
+++ app.js
@@ -57,6 +57,19 @@ var budgetController = (function() {
 			return newItem;
 		},
 
+		deleteItem: function(type, id) {
+			var ids, index;
+			ids = data.allItems[type].map(function(current) {
+				return current.id;
+			});
+
+			index = ids.indexOf(id);
+
+			if (index !== -1) {
+				data.allItems[type].splice(index, 1);
+			}
+		},
+
 		calculateBudget: function() {
 			// 1. Calculate total income and expenses
 			calculateTotal('exp');
@@ -221,11 +234,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
 		if (itemID) {
 			// inc-1
-			sliptID = itemID.split('-');
+			splitID = itemID.split('-');
 			type = splitID[0];
-			ID = splitID[1];
+			ID = parseInt(splitID[1]);
 
 			// 1. Delete item from data structure
+			budgetCtrl.deleteItem(type, ID);
 
 			// 2. Delete Item from UI
 
